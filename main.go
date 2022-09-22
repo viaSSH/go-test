5704f9294a07d4c85740c41b0a34bae4ba3fb53a
viaSSH <hanseung0609@gmail.com>
2022-09-22 14:02:52 +0900

    Add multi function

--- main.go
+++ main.go
@@ -9,14 +9,17 @@ func Calc(a int, b int) int {
 	return a + b
 }
 
+func Multi(a int, b int) int {
+	return a * b
+}
+
 func main() {
 
 	fmt.Println("THIS IS TEST CODE")
 
 	var c = Calc(1, 2)
+	var d = Multi(3, 4)
 
-	fmt.Println(c)
-
-	fmt.Println("this is new part")
+	fmt.Println(c, d)
 
 }
