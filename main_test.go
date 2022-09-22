3ebb0a709f20707341e59b92be1ee571aa688f7d
viaSSH <hanseung0609@gmail.com>
2022-09-22 14:03:08 +0900

    Add multi Test Code

--- main_test.go
+++ main_test.go
@@ -14,8 +14,16 @@ func TestCalc(t *testing.T) {
 
 func TestMyTest(t *testing.T) {
 	var a int = 1
-	var b int = 2
+	var b int = 1
 	if a != b {
 		t.Error("miss match")
+	}
+}
+
+func TestMulti(t *testing.T) {
+	s := Multi(4, 5)
+
+	if s != 20 {
+		t.Error("Wrong Multiple")
 	}
 }
